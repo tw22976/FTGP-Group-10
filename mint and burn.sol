3e3bcdd04ea39e40631bb2c8176cc5237e283a1f
Visal Hewagama <68350013+visal-h@users.noreply.github.com>
2023-04-20 21:34:51 +0100

    Add files via upload

---
+++ mint and burn.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MyToken is ERC20, Ownable {
+    constructor(uint256 initialSupply) ERC20("My Token", "AYT") {
+        _mint(msg.sender, initialSupply);
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(address from, uint256 amount) public onlyOwner {
+        _burn(from, amount);
+    }
+
+    function _transfer(address sender, address recipient, uint256 amount) internal virtual override {
+        require(sender == owner() || recipient == owner(), "MyToken: transfers are disabled");
+        super._transfer(sender, recipient, amount);
+    }
+}
